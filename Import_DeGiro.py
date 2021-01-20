17dbb2638dadafd032b822b495f78f9ba763097c
Timo Oosterveer <oosterveer.timo@gmail.com>
2021-01-20 17:29:54 +0100

    Update Import_DeGiro.py
    First steps of handeling the DeGiro export

--- Import_DeGiro.py
+++ Import_DeGiro.py
@@ -11,4 +11,20 @@ Goal: Automatically read all transactions and calculate current portfolio, inclu
     Dividend income
 
 """
+import pandas as pd #import pandas to import excel file into pyhton
 
+#import your exported .xls file and call it trans.
+#insert your own personal paht to your xls file:
+trans = pd.read_excel (r'Your\path\to\your\xls\file\filename.xls')
+
+#remove all rows which does not include an Order Id:
+trans.dropna(subset=['Order Id'], inplace=True)
+
+#rename columns:
+trans.columns = ['date', 'del0', 'del1', 'product', 'ISIN', 'description',
+              'del2', 'valuta', 'value', 'del3', 'del4', 'del5']
+
+#remove colums which are unnecesary (named 'del#'):
+trans = trans[trans.columns[~trans.columns.str.contains('del')]]
+
+print(trans)
