bbabbff8dfd8dc7f54eb9d5c2e5802795bd772cb
Timo Oosterveer <oosterveer.timo@gmail.com>
2021-01-20 16:36:14 +0100

    Create Import_DeGiro.py

---
+++ Import_DeGiro.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 16:32:44 2021
+
+This file can be used to import an overview of your transactions
+
+Goal: Automatically read all transactions and calculate current portfolio, including:
+    Number of effects
+    GAK
+    Transaction costs
+    Dividend income
+
+"""
+
