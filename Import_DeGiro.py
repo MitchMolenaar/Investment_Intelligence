5a5a6aa5c1fa7c477035f49c851acfd2490adc51
Timo Oosterveer <oosterveer.timo@gmail.com>
2021-01-23 16:53:14 +0100

    simple_dashboard function
    Import xls from De Giro and create simple plot

--- Import_DeGiro.py
+++ Import_DeGiro.py
@@ -11,28 +11,67 @@ Goal: Automatically read all transactions and calculate current portfolio, inclu
     Dividend income
 
 """
-import pandas as pd #import pandas to import excel file into pyhton
+#imports
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#import your exported .xls file and call it trans.
-#insert your own personal paht to your xls file:
-trans = pd.read_excel (r'C:\Users\ttmoo\Documents\Administratie\Privé\Github\Account.xls')
+def simple_dashboard(your_xls_file):
+    """
+    Parameters
+    your_xls_file : type = STRING
+        r'C:\your\path\to\your\degiro\export\filename.xls'
 
-#remove all rows which does not include an Order Id:
-trans.dropna(subset=['Order Id'], inplace=True)
+    Returns
+    Simple graph of your shares in De Giro, GAK and total money invested
+    Pandas DataFrame calles 'total' with all transaction in it
+    
+    """
+    #import your exported .xls file and call it trans.
+    #insert your own personal paht to your xls file:
+    trans = pd.read_excel (your_xls_file)
 
-#rename columns:
-trans.columns = ['date', 'del0', 'del1', 'product', 'ISIN', 'description',
-              'del2', 'valuta', 'value', 'del3', 'del4', 'del5']
+    #remove all rows which does not include an Order Id:
+    trans.dropna(subset=['Order Id'], inplace=True)
 
-#remove colums which are unnecesary (named 'del#'):
-trans = trans[trans.columns[~trans.columns.str.contains('del')]]
-trans = trans.reset_index() #reset indices
-trans["description"]= trans["description"].str.split(" ", n = 2, expand = False) 
+    #rename columns:
+    trans.columns = ['date', 'del0', 'del1', 'product', 'ISIN', 'description',
+                     'del2', 'valuta', 'value', 'del3', 'del4', 'del5']
 
-# iterate through each row and select  
-# 'Name' and 'Age' column respectively. 
-for i in range(len(trans)) : 
-    print(trans.loc[i, "product"], trans.loc[i, "description"], trans.loc[i, "value"])
+    #remove colums which are unnecesary (named 'del#'):
+    trans = trans[trans.columns[~trans.columns.str.contains('del')]]
+    trans = trans.reset_index() #reset indices
+
+    #extract number of products bought, price per product, products bought and their initial value
+    trans_des = trans["description"].str.split(" ", n = -1, expand = True)
+    num_products = trans_des[1]
+    pric_pp = trans_des[3]
     products = trans["product"]
-    num_products = trans["description"]
-    values = trans["value"]*(-1)
+    ISIN = trans["ISIN"]
+    values = trans["value"]
+
+    #combine products in one dataframe
+    global total #global makes it possible to return total
+    total = pd.concat([products, ISIN, num_products, pric_pp, values], axis=1) 
+    total.columns=["product", "isin", "number", "ini_value", "total_value"]
+
+    #creating subplots
+    fig = plt.figure(figsize=(10,5))
+    sub1 = fig.add_subplot(131)
+    sub2 = fig.add_subplot(132)
+    sub3 = fig.add_subplot(133)
+
+    #plot the data and set lables
+    sub1.barh(total["isin"], total["number"]) #number of shares
+    sub2.barh(total["isin"], total["ini_value"]) #mean purchase value
+    sub3.barh(total["isin"], total["total_value"]*(-1)) #total money invested
+    sub1.set_xlabel('# of shares')
+    sub2.set_xlabel('GAK (€)')
+    sub2.axes.get_yaxis().set_visible(False) #hide ylabel
+    sub2.set_title('Overview of De Giro portfolio')
+    sub3.set_xlabel('Total invested (€)')
+    sub3.axes.get_yaxis().set_visible(False) #hide ylabel
+    
+    return total
+
+#run simple_dashboard function
+simple_dashboard(r'C:\Users\ttmoo\Documents\Administratie\Privé\Github\Account.xls')
