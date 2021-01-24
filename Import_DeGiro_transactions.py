295bed29a2ba98ce3fac9245d87bf20e0e8164f8
molenaarma <mitchmolenaar@gmail.com>
2021-01-24 18:42:39 +0100

    Update Import_DeGiro_transactions.py
    Current value calculated with closing price (scraping from yahoo finance)

--- Import_DeGiro_transactions.py
+++ Import_DeGiro_transactions.py
@@ -10,6 +10,10 @@ Goal: Automatically read all transactions and calculate current portfolio, inclu
     Transaction costs
     Dividend income
 
+To do:
+    - winst bereken: huidige waarde aandeel - aankoop waarde
+    - wisselkoersen toevoegen bij kopen aandeel buitenlandse markt.
+
 """
 #imports
 import pandas as pd
@@ -27,16 +31,33 @@ Pandas DataFrame calles 'total' with all transaction in it
 
 """
 
-def simple_dashboard(your_csv_file_transactions, your_csv_file_account):
+def simple_dashboard(your_csv_file_transactions, your_csv_file_account, companies):
 #import your exported .csv files and call them Transactions and Account.
 #insert your own personal paths to your files:
 #simpledashboard(your_csv_file_transactions,your_csv_file_account)
+#import your stocks/companies as shown below. THe abbreviations are from Yahoo finance.
     transaction =  pd.read_csv(your_csv_file_transactions) 
     account = pd.read_csv(your_csv_file_account)
     
     #remove all rows which does include an Order Id:
     #transaction.drop(['Order ID'], axis=1, inplace=True)
     #account.drop(['Order Id'], axis=1, inplace=True)
+    
+    comp=pd.DataFrame(); # make a dataframe for companies
+    for company in companies: #iterate over the companies
+        print(company)
+        statistics = pd.read_html(f'https://finance.yahoo.com/quote/{company[0]}/key-statistics?p={company[0]}'); #read statistics from yahoo finance
+        statistics2 = pd.read_html(f'https://finance.yahoo.com/quote/{company[0]}?p={company[0]}'); #read statistics from yahoo finance
+        prices = statistics2[0]; #extract prices from yahoo finance
+        summary = statistics2[1]; # extract data from tab summary in yahoo finance
+        comp1=pd.DataFrame({"Stock":[company[1]], 
+                          "Stock_abr":[company[0]],
+                          "Sector":[company[2]],
+                          "Close_price":[prices[1][0]],
+                          }); # make dataframe with the stock, the abbreviation of the stock, the sector and the close price.
+        comp=comp.append(comp1); #append dataframe for each stock
+    comp = comp.set_index('Stock') # set index as stock name   
+    comp['Close_price']=comp['Close_price'].astype(str).astype(float) # transform close price to a float/number.
 
     #make dataframe portfolio
     portfolio=[]
@@ -47,13 +68,16 @@ def simple_dashboard(your_csv_file_transactions, your_csv_file_account):
         Total_value_specific_stock = sum(specific_stock.Totaal) # summate the total vallue of that stock
         Sold_value = sum(specific_stock.Totaal[specific_stock.Totaal >= 0])  #summate for how much you have sold the stocks
         Bought_value = sum(specific_stock.Totaal[specific_stock.Totaal < 0]) #summate for how much you have bought the stocks
-        Stock_in_portfolio=pd.DataFrame({'Stock': [stock], 'Current number of stocks':[Total_number_specific_stock], 
-                                         'Value of stocks':[Total_value_specific_stock], 'Bought':[Bought_value], 'Sold':[Sold_value]}) #put values in dataframe
+        Stock_in_portfolio=pd.DataFrame({'Stock': [stock], 'Current number of stocks':[Total_number_specific_stock],
+                                         'Bought':[Bought_value], 'Sold':[Sold_value],
+                                         'Difference bought/sold':[Total_value_specific_stock]}) #put values in dataframe
         portfolio.append(Stock_in_portfolio) # append the data of all stocks
         print(stock)
     
     portfolio = pd.concat(portfolio) # put all stocks in one dataframe
     portfolio = portfolio.set_index('Stock') # set index as stock name
+    portfolio = comp.join(portfolio)
+    portfolio['Current value Stock'] = portfolio['Close_price'] * portfolio['Current number of stocks']
     
     #creating subplots
     fig = plt.figure(figsize=(10,5))
@@ -63,8 +87,8 @@ def simple_dashboard(your_csv_file_transactions, your_csv_file_account):
 
     #plot the data and set lables
     sub1.barh(portfolio.index, portfolio["Current number of stocks"], height=0.75) #number of shares
-    sub2.barh(portfolio.index, (portfolio["Value of stocks"]*(-1)/portfolio["Current number of stocks"]), height=0.75) #mean purchase value
-    sub3.barh(portfolio.index, portfolio["Value of stocks"]*(-1), height=0.75) #total money invested
+    sub2.barh(portfolio.index, (portfolio["Difference bought/sold"]*(-1)/portfolio["Current number of stocks"]), height=0.75) #mean purchase value
+    sub3.barh(portfolio.index, portfolio["Difference bought/sold"]*(-1), height=0.75) #total money invested
     sub1.set_xlabel('# of stocks')
     sub2.set_xlabel('GAK (€)')
     sub2.axes.get_yaxis().set_visible(False) #hide ylabel
@@ -72,14 +96,31 @@ def simple_dashboard(your_csv_file_transactions, your_csv_file_account):
     sub3.set_xlabel('Total invested (€)')
     sub3.axes.get_yaxis().set_visible(False) #hide ylabel
     
-    return portfolio
+    return portfolio, comp
 
 
-check_portfolio_of='Mitch'
+
+
+check_portfolio_of='Mitch' ## fill in name of person
+
 if check_portfolio_of=='Timo':
-    portfolio=simple_dashboard(r'C:\Users\ttmoo\Documents\Administratie\Privé\Github\Transactions.csv',
-                           r'C:\Users\ttmoo\Documents\Administratie\Privé\Github\Account.csv')
+    companies = [['PHARM.AS', 'PHARMING GROUP', 'Health care']]
+    [portfolio,comp]=simple_dashboard(r'C:\Users\ttmoo\Documents\Administratie\Privé\Github\Transactions.csv',
+                           r'C:\Users\ttmoo\Documents\Administratie\Privé\Github\Account.csv', companies)
+
+
 if check_portfolio_of=='Mitch':
-    portfolio=simple_dashboard(r'C:\Users\mitch\Documents\GitHub\Investment_Intelligence\Transactions.csv',
-                           r'C:\Users\mitch\Documents\GitHub\Investment_Intelligence\Account.csv')
+    companies = [['IBM', 'INTERNATIONAL BUSINESS', 'Technology'],
+                 ['GLPG.AS', 'GALAPAGOS', 'Health Care'], 
+                 ['IEUR', 'ISHARES MSCI EUR A', 'ETF'], 
+                 ['RDSA.AS', 'ROYAL DUTCH SHELLA', 'Energy'],
+                 ['PHARM.AS', 'PHARMING GROUP', 'Health care'],
+                 ['GOOG', 'ALPHABET INC. - CLASS A', 'Technology'],
+                 ['ASML.AS', 'ASML HOLDING', 'Technology'],
+                 ['DSM.AS', 'DSM KON', 'Technology']]
+    
+    [portfolio,comp]=simple_dashboard(r'C:\Users\mitch\Documents\GitHub\Investment_Intelligence\Transactions.csv',
+                           r'C:\Users\mitch\Documents\GitHub\Investment_Intelligence\Account.csv', companies)
+               
+          
 
